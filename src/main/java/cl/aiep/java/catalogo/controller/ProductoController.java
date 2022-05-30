144e9fa4c71519954f03154fef947e6c08715b17
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-05-30 12:35:12 -0400

    sr creo vista de lista, formulario para la clase producto, con sus metodos en el controlle. Queda realziar el mapeo para editar y eliminar los productos y como agregado el cargar las imagenes a la base de datos

--- src/main/java/cl/aiep/java/catalogo/controller/ProductoController.java
+++ src/main/java/cl/aiep/java/catalogo/controller/ProductoController.java
@@ -1,10 +1,17 @@
 package cl.aiep.java.catalogo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cl.aiep.java.catalogo.modelo.Categoria;
+import cl.aiep.java.catalogo.modelo.Producto;
+import cl.aiep.java.catalogo.repository.CategoriaRepository;
 import cl.aiep.java.catalogo.repository.ProductoRepository;
 
 
@@ -15,11 +22,30 @@ public class ProductoController {
 	@Autowired
 	ProductoRepository productoRepository;
 	
+	@Autowired
+	CategoriaRepository categoriaRepository;
 	
 	@GetMapping("/nuevo")
-	public String nuevoRegistro() {
-		return "hola";
+	public String nuevoRegistro(Producto producto, Model modelo) {
+		List<Categoria> categorias = categoriaRepository.findAll();
+		modelo.addAttribute("categorias", categorias);
+		return "producto/formulario";
 	}
+	
+	@PostMapping("/registrar")
+	public String registrar(Producto producto) {
+		productoRepository.saveAndFlush(producto);
+		return "redirect:/producto/lista";
+	}
+	
+	
+	@GetMapping("/lista")
+	public String listar(Model modelo) {
+		List<Producto> productos = productoRepository.findAll();
+		modelo.addAttribute("productos", productos);
+		return "producto/lista";
+	}
+	
 	
 	
 }
